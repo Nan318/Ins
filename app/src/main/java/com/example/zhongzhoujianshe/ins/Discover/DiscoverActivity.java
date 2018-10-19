bb8d6badebe9af0d93df85d7e18350250634927b
Nan318 <nzhang5@student.unimelb.edu.au>
2018-10-20 05:07:33 +1100

    search user

--- app/src/main/java/com/example/zhongzhoujianshe/ins/Discover/DiscoverActivity.java
+++ app/src/main/java/com/example/zhongzhoujianshe/ins/Discover/DiscoverActivity.java
@@ -1,20 +1,34 @@
 package com.example.zhongzhoujianshe.ins.Discover;
 
-import android.support.annotation.NonNull;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.zhongzhoujianshe.ins.Helper.BottomNavigationViewHelper;
+import com.example.zhongzhoujianshe.ins.Helper.User;
 
 import com.example.zhongzhoujianshe.ins.R;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiscoverActivity extends AppCompatActivity {
     // Firebase instance variables
@@ -22,54 +36,123 @@ public class DiscoverActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseDatabase mDb;
     private DatabaseReference mRef;
-
     //variable
     private String currentUserId;
+    private static final int ACTIVITY_NUM = 1;
+    private Context mContext;
+    private List<User> userList;
+    private UserListAdapter mAdapter;
+    //UI objects
+    private EditText et_search;
+    private ListView listView;
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
 
-        /* * * * * firebase * * * * * */
-        //get Uid
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    currentUserId = user.getUid();
-                }
-            }
-        };
+        mContext = DiscoverActivity.this;
 
         /* * * * * initialize view * * * * * */
-        initialView();
 
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        et_search = (EditText) findViewById(R.id.search);
+        listView = (ListView) findViewById(R.id.listView);
+        //Log.e("DISCOVER", "onCreate: started.");
+
+        //hide SoftKeyboard
+        if(getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+
+        //set up the bottom navigation view
+        Log.e("SET", "bottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottom_navigation_view);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this,bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+
+
+        //initTextListener();
+
+        Log.e("DISCOVER", "initTextListener: initializing");
+
+        userList = new ArrayList<>();
+
+        et_search.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                mAuth.signOut();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                //String text = et_search.getText().toString().toLowerCase(Locale.getDefault());
+                String text = et_search.getText().toString();
+                searchForMatch(text);
             }
         });
 
+
     }
 
-    private void initialView() {
+    private void searchForMatch(String keyword){
+        Log.e("DISCOVER", "searchForMatch: searching for a match: " + keyword);
+        userList.clear();
+        //update the users list view
+        if(keyword.length() != 0){
+            DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+            Query query = mRef.child("users")
+                    .orderByChild("username").equalTo(keyword);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+                        Log.d("DISCOVER", "onDataChange: found user:" + singleSnapshot.getValue(User.class).toString());
+
+                        userList.add(singleSnapshot.getValue(User.class));
+                        //update the users list view
+                        updateListView();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
     }
+
+    //update the list view
+    //i.e. show the user list
+    private void updateListView(){
+        Log.e("DISCOVER", "updateListView: updating users list");
+
+        //mAdapter = new UserListAdapter(DiscoverActivity.this, R.layout.layout_user_listitem, userList);
+
+        mAdapter = new UserListAdapter(mContext, userList);
+        listView.setAdapter(mAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("DISCOVER", "onItemClick: selected user: " + userList.get(position).toString());
+
+                //navigate to profile activity
+                /*
+                Intent intent =  new Intent(DiscoverActivity.this, ProfileActivity.class);
+                intent.putExtra(getString(R.string.calling_activity), getString(R.string.search_activity));
+                intent.putExtra(getString(R.string.intent_user), userList.get(position));
+                startActivity(intent);*/
+                Log.e("NAVIGATION", "go to Profile");
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/zhongzhoujianshe/ins/Discover/UserListAdapter.java
@@ -0,0 +1,137 @@
+package com.example.zhongzhoujianshe.ins.Discover;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.zhongzhoujianshe.ins.Helper.User;
+import com.example.zhongzhoujianshe.ins.Helper.UserProfileModel;
+import com.example.zhongzhoujianshe.ins.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by User on 9/17/2017.
+ */
+
+public class UserListAdapter extends BaseAdapter {
+
+    private List<User> users = null;
+    private Context mContext;
+
+    public UserListAdapter(@NonNull Context context, @NonNull List<User> users) {
+        this.mContext = context;
+        this.users = users;
+    }
+
+    private static class ViewHolder{
+        TextView txt_username;
+        TextView txt_email;
+        CircleImageView profilePhoto;
+    }
+
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public User getItem(int position) {
+        return users.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View view, @NonNull ViewGroup viewGroup) {
+
+
+        final ViewHolder holder;
+
+        if(view == null){
+            holder = new ViewHolder();
+            //view = mInflater.inflate(layoutResource, viewGroup, false);
+            view = LayoutInflater.from(mContext).inflate(R.layout.layout_user_listitem,viewGroup,false);
+
+            holder.txt_username = (TextView) view.findViewById(R.id.username);
+            holder.txt_email = (TextView) view.findViewById(R.id.email);
+            holder.profilePhoto = (CircleImageView) view.findViewById(R.id.profile_image);
+
+            view.setTag(holder);
+        }else{
+            holder = (ViewHolder) view.getTag();
+        }
+
+        holder.txt_username.setText(getItem(position).getUsername());
+        holder.txt_email.setText(getItem(position).getEmail());
+        String itemUserId = getItem(position).getUserId();
+
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+        Query query = mRef.child("user_setting").orderByChild("userId").equalTo(itemUserId);
+        //display profile photo for user
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot: dataSnapshot.getChildren()){
+
+                    ImageLoader imageLoader = ImageLoader.getInstance();
+
+                    imageLoader.displayImage(singleSnapshot.getValue(UserProfileModel.class).getProfilePhoto(),
+                            holder.profilePhoto);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return view;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
