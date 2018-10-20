83178ed3554eb6b5f7656608c991cbb26ac20b5b
kahunsha <wanqiul@student.unimelb.edu.au>
2018-10-21 07:02:09 +1100

    fasfsf

---
+++ app/src/main/java/com/example/zhongzhoujianshe/ins/Home/displaylike.java
@@ -0,0 +1,15 @@
+package com.example.zhongzhoujianshe.ins.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zhongzhoujianshe.ins.R;
+
+public class displaylike extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaylike);
+    }
+}
